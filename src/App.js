772c84d15991eaf8fd6ed0b27c8315f79cabdebc
Jiaqi <dxq7nz@virginia.edu>
2023-02-26 23:19:19 -0500

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+export default function App() {
+  const [result, setResult] = useState("");
+  async function fetchInfo() {
+    const url = `${process.env.REACT_APP_API_URL}/test`;
+    const response = await fetch(url);
+    const json = await response.json();
+    setResult(json);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={fetchInfo}>fetch data from api</button>
+      <div>result: {result}</div>
     </div>
   );
 }
-
-export default App;
